31e7ba380a32f0e811a387ac82888953a0754e29
kirthi0103 <147997127+kirthi0103@users.noreply.github.com>
2025-08-07 18:45:02 +0530

    Add files via upload

---
+++ day2/NegativeNumber.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class NegativeNumber {
+	public static void main(String[] args) {
+		int number = -40;
+		if(number>0) {
+			System.out.println("The number is positive");
+		}else {
+			System.out.println("The number "+number +" is negative");
+		}
+	}
+
+}
